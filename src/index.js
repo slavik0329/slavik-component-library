3541b059f25a3e71f4a36dec33cbb706c2b82ceb
Steve Dakh <slavik0329@gmail.com>
2016-11-02 10:46:47 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import PageContainer from './components/PageContainer';
+import DataBlock from './components/DataBlock';
+import BtcTicker from './components/BtcTicker';
 
-export {PageContainer};
+export {PageContainer, DataBlock, BtcTicker};
